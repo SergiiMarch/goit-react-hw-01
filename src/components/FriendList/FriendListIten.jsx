eb152b7a01ea6c6295d405c5407fdee1d625b36f
Sergii Marchenko <marikmarchelo@gmail.com>
2024-08-08 21:52:57 +0300

    add FriendListIten

--- src/components/FriendList/FriendListIten.jsx
+++ src/components/FriendList/FriendListIten.jsx
@@ -3,15 +3,16 @@ import styles from "./FriendList.module.css";
 export default function FriendListItem({ avatar, name, isOnline }) {
   return (
     <div className={styles.box}>
-      <img src={avatar} alt={name} width="240px" />
+      <img src={avatar} alt={name} width="40px" />
       <p
         style={{
           fontWeight: "bold",
           fontSize: "27px",
           marginBottom: "20px",
+          color: "black",
         }}
       >
-        Name: {name}
+        {name}
       </p>
       <p className={isOnline ? styles.isActive : styles.isRetired}>
         {isOnline ? "Online" : "Offline"}
